80798d055b7787118746f3781d945ca83f08aa4a
Jesse Meirink <jesse@meirink.com>
2022-06-16 16:49:28 -0500

    change css and refactor routes skills.js

--- routes/skills.js
+++ routes/skills.js
@@ -3,25 +3,12 @@ import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
-// GET -- localhost:3000/skills
 router.get('/', skillsCtrl.index)
-
-// GET -- localhost:3000/skills/new
 router.get('/new', skillsCtrl.new)
-
-// GET -- localhost:3000/skills/:id
 router.get('/:id', skillsCtrl.show)
-
-// GET -- localhost:3000/skills/:id/edit
 router.get('/:id/edit', skillsCtrl.edit)
-
-// POST -- localhost:3000/skills
 router.post('/', skillsCtrl.create)
-
-// DELETE -- localhost:3000/skills/:id
 router.delete('/:id', skillsCtrl.delete)
-
-// PUT -- localhost:3000/skills/:id
 router.put('/:id', skillsCtrl.update)
 
 
